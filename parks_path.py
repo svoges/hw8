6520ef68484d77900155e4db0ceb094f6fa76063
Steffan Voges <steffan@berkeley.edu>
2014-10-30 15:48:04 -0700

    first commit for parks, using floyd marshall

---
+++ parks_path.py
@@ -0,0 +1,8 @@
+def algorithm(G, l, k):
+    dist = [V][V]
+    for each vertex v in V:
+        dist[[v][v] = 0 #distance from v to v is 0
+    for each edge (u, v) in l:
+        dist[u][v] = l[u][v]
+    for k = 1...|V|:
+        
