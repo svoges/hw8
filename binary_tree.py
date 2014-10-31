85e00465cbf50842a6afa78832e2a51f9bf46cf0
Steffan Voges <steffan@berkeley.edu>
2014-10-31 13:48:43 -0700

    pseudo code and main idea finished

--- binary_tree.py
+++ binary_tree.py
@@ -1,68 +1,62 @@
-from itertools import chain
-from random import randint
+def BinaryTree(r):
+    return [r, [], []]
+
+def insertLeft(root,newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1,[newBranch,t,[]])
+    else:
+        root.insert(1,[newBranch, [], []])
+    return root
+
+def insertRight(root,newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2,[newBranch,[],t])
+    else:
+        root.insert(2,[newBranch,[],[]])
+    return root
+
+def getRootVal(root):
+    return root[0]
+
+def setRootVal(root,newVal):
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
+if __name__ == "__main__":
+    freq = {"begin": .05,
+            "do"   : .4,
+            "else" : .08,
+            "end"  : .04,
+            "if"   : .1,
+            "then" : .1,
+            "while": .23}
+    r = BinaryTree(max(freq, key=freq.get))
+    print(r[0])
+    del(freq[r[0]])
+    dleft = dict(freq.items())
+    print(dleft)
 
 
-def leaf(t):
-    return isinstance(t, str)
-
-def random(n):
-    def extend(t):
-        if leaf(t):
-            return (t+'l', t+'r')
-        else:
-            l, r = t
-            if randint(0, 1): return (l, extend(r))
-            else: return (extend(l), r)
-    t = ''
-    for _ in range(n-1): t = extend(t)
-    return t
-
-def format(t):
-    def pad(prefix, spaces, previous):
-        return prefix + (' ' * spaces) + previous
-    def merge(l, r):
-        l_above, l_below, l_lines = l
-        r_above, r_below, r_lines = r
-        gap = r_below + l_above
-        gap_above = l_above
-        gap_below = gap - gap_above
-        def lines():
-            for (i, line) in enumerate(chain(r_lines, l_lines)):
-                if i < r_above:
-                    yield ' ' + line
-                elif i - r_above < gap_above:
-                    dash = '_' if i - r_above == gap_above - 1 else ' '
-                    if i < r_above + r_below:
-                        yield pad(dash + '/', 2 * (i - r_above), line)
-                    else:
-                        yield pad(dash + '/', 2 * gap_below - 1, line)
-                elif i - r_above - gap_above < gap_below:
-                    if i < r_above + r_below:
-                        yield pad(' \\', 2 * gap_above - 1, line)
-                    else:
-                        spaces = 2 * (r_above + gap_above + gap_below - i - 1)
-                        yield pad(' \\', spaces, line)
-                else:
-                    yield ' ' + line
-        return (r_above + gap_above, gap_below + l_below, lines())
-    def descend(left, t):
-        if leaf(t):
-            if left:
-                return (1, 0, [t])
-            else:
-                return (0, 1, [t])
-        else:
-            l, r = t
-            return merge(descend(True, l), descend(False, r))
-    def flatten(t):
-        above, below, lines = t
-        for (i, line) in enumerate(lines):
-            if i < above: yield (' ' * (above - i - 1)) + line
-            else: yield (' ' * (i - above)) + line
-    return '\n'.join(flatten(descend(True, t)))
 
 
-if __name__ == '__main__':
-    for n in range(1,20,3):
-        tree = random(n)
-        print(format(tree))
+
+# r = BinaryTree(3)
+# insertLeft(r,4)
+# insertLeft(r,5)
+# insertRight(r,6)
+# insertRight(r,7)
+# l = getLeftChild(r)
+# print(l)
+#
+# setRootVal(l,9)
+# print(r)
+# insertLeft(l,11)
+# print(r)
+# print(getRightChild(getRightChild(r)))
